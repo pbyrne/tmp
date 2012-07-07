62025d46ed8230a624d0fff383b19e14d2420f54
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-07-07 15:06:39 -0500

    Trim out even more of the extra lines

--- lpthw/ex17.py
+++ lpthw/ex17.py
@@ -3,19 +3,7 @@ from os.path import exists
 
 script, from_file, to_file = argv
 
-print "Copying from %s to %s" % (from_file, to_file)
-
 indata = open(from_file).read()
-
-print "The imnput file is %d bytes log" % len(indata)
-
-print "Does the output file exist? %r" % exists(to_file)
-print "Ready, hit RETURN to continue, CTRL-C to abort"
-raw_input()
-
-output = open(to_file, 'w')
-output.write(indata)
-
-print "Alright, all done."
-
-output.close()
+outfile = open(to_file, 'w')
+outfile.write(indata)
+outfile.close()
