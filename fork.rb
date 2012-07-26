490e5accf44cfb1b8bfc6bfeee53cb6dac722d0f
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-07-26 14:03:24 -0500

    testing how to deal with forks in forks

--- fork.rb
+++ fork.rb
@@ -4,6 +4,10 @@ def say message
   puts "PID #{pid} : #{stamp} : #{message}"
 end
 
+def sleep_a_bit(mult = 2)
+  sleep rand * mult
+end
+
 say "Starting script"
 
 # if fork
@@ -13,23 +17,46 @@ say "Starting script"
 #   say "This is from the child"
 # end
 
-servers = []
+# servers = []
 
-10.times do
+# 10.times do
+#   fork do
+#     sleep rand * 2
+#     value = rand(100)
+#     say "This is from the child. Random value of #{value} selected."
+#     servers << value
+#     say servers
+#   end
+# end
+
+# say "This happens while the children are running"
+# say "The current servers list is: #{servers}"
+
+# Process.waitall
+
+# say "This happens only after all the children are done"
+# say "The current servers list is: #{servers}"
+
+# ok, so can we fork inside a fork?
+fork do
+  say "Begin outer fork"
+
   fork do
-    sleep rand * 2
-    value = rand(100)
-    say "This is from the child. Random value of #{value} selected."
-    servers << value
-    say servers
+    sleep_a_bit
+    say "Inner fork A"
   end
+
+  fork do
+    sleep_a_bit
+    say "Inner fork B"
+  end
+
+  say "End outer fork"
+
+  Process.waitall
+
+  say "Inner forks completed"
 end
 
-say "This happens while the children are running"
-say "The current servers list is: #{servers}"
-
 Process.waitall
-
-say "This happens only after all the children are done"
-say "The current servers list is: #{servers}"
 
