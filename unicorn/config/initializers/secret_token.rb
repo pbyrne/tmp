7e473f18b80ebb50a48be9179d046c67c50391e4
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-08-17 11:08:49 -0500

    new rails app

---
+++ unicorn/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unicorn::Application.config.secret_token = '23c4ad6597044337d0dbca23ebc4b5ae0aa986449c2565544d18a5340be5f5bd46178a8e4f1b8d8993d3088b7d1987251c5775fbe83459d27b3afb355f6e8e09'
