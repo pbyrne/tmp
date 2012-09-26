c56f9c4cf1d4a51102e6846cf50439c4960447e0
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-09-26 08:40:50 -0500

    add bare rails 3.2.8 app to test pluck

---
+++ testpluck/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testpluck::Application.config.secret_token = '0a2bd25f138bd25d710032cb4c11ac28aacd1c8d8c33bac6211b68c2653d450af6f1728391bba57f02bc992c15317c44706ec908da1acc65c3bec84269556b37'
