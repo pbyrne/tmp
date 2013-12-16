637f07a41b248100886baf165b4bbcb20f211c13
Patrick Byrne <code@patrickbyrne.net>
2013-12-15 21:10:05 -0600

    Session 5 of Go By Example

---
+++ gobyexample.com/random-numbers.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+import "math/rand"
+
+func main() {
+  fmt.Print(rand.Intn(100), ".")
+  fmt.Print(rand.Intn(100))
+  fmt.Println()
+
+  fmt.Println(rand.Float64)
+
+  fmt.Print((rand.Float64()*5), ".")
+  fmt.Print((rand.Float64()*5) + 5)
+  fmt.Println()
+
+  s1 := rand.NewSource(42)
+  r1 := rand.New(s1)
+
+  fmt.Print(r1.Intn(100), ".")
+  fmt.Print(r1.Intn(100))
+  fmt.Println()
+
+  fmt.Println(r1.Float64)
+
+  fmt.Print((r1.Float64()*5), ".")
+  fmt.Print((r1.Float64()*5) + 5)
+  fmt.Println()
+
+}
---
+++ gobyexample.com/time-formatting-parsing.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+  p := fmt.Println
+
+  t := time.Now()
+  p(t.Format("2006-01-02T15:04:05Z07:00"))
+  p(t.Format("3:04PM"))
+  p(t.Format("Mon Jan _2 15:04:05 2006"))
+  p(t.Format("2006-01-02T15:04:05.999999-07:00"))
+
+
+  fmt.Printf("%d-%02d-%02dT%02d:%02d:%02d-00:00\n",
+    t.Year(), t.Month(), t.Day(),
+    t.Hour(), t.Minute(), t.Second())
+
+  withNanos := "2006-01-02T15:04:05.999999999-07:00"
+  t1, e := time.Parse(
+    withNanos,
+    "2012-11-01T22:08:41.117442+00:00")
+  p(t1)
+
+  kitchen := "3:04PM"
+  t2, e := time.Parse(kitchen, "8:41PM")
+  p(t2)
+
+  ansic := "Mon Jan _2 15:04:05 2006"
+  _, e = time.Parse(ansic, "8:41PM")
+  p(e)
+
+  p(t.Format(time.Kitchen))
+
+}
