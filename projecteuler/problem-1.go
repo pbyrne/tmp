69294df4dbf1cd723349d1b331839c4bbb407a23
Patrick Byrne <code@patrickbyrne.net>
2014-10-14 09:58:41 -0500

    Extract out a function from main for problem 1

--- projecteuler/problem-1.go
+++ projecteuler/problem-1.go
@@ -9,13 +9,18 @@ func main() {
   sum := 0
 
   for i := 0; i < ceiling; i++ {
-    if i % 3 == 0 || i % 5 == 0 {
-      sum = sum + i
-      fmt.Println("Current sum is", sum)
-    }
+    sum = sum + addIfMultiple(i)
+    fmt.Println("Current sum is", sum)
   }
 
   fmt.Println("\n\nTotal sum is", sum)
 }
 
+func addIfMultiple(i int) int {
+  if i % 3 == 0 || i % 5 == 0 {
+    return i
+  } else {
+    return 0
+  }
+}
 
