c8c29af48bc6d82b94e5a7e24ac7998bf65e6968
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2017-08-21 07:52:03 -0500

    Further going through the exercises

---
+++ programming-elixir/issues/lib/issues/git_hub_issues.ex
@@ -0,0 +1,22 @@
+defmodule Issues.GitHubIssues do
+  @github_url Application.get_env(:issues, :github_url)
+  @user_agent [ {"User-agent", "Elixir dave@pragprog.com"} ]
+
+  def fetch(user, project) do
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
+  end
+
+  def issues_url(user, project) do
+    "https://#{@github_url}/repos/#{user}/#{project}/issues"
+  end
+
+  def handle_response({ :ok, %{status_code: 200, body: body} }) do
+    { :ok, Poison.Parser.parse!(body) }
+  end
+
+  def handle_response({ _, %{status_code: _, body: body} }) do
+    { :error, Poison.Parser.parse!(body) }
+  end
+end
